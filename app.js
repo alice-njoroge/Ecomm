72becbb03a24258e2ac78f952c52691a0bfe7233
Alice <alicenjoroge707@gmail.com>
2020-02-08 20:30:22 +0300

    add items to local storage

--- app.js
+++ app.js
@@ -18,18 +18,29 @@ fetch('products.json')
         });
         // send data to the UI
         document.querySelector('.products-center').innerHTML = productsHtmlString;
-        // add item to cart
+        // get item id on click
         let addToBagButton = document.querySelectorAll('.bag-btn');
         addToBagButton.forEach(button => {
-            button.addEventListener('click',event=>{
-                let button = event.target;
-                button.innerText = 'Added to Bag';
-                button.disabled= true;
-               let productId =  button.dataset.id;
-               let product = products.find(product => {
-                   return product.id === productId;
-               });
-                console.log(product.price);
+            button.addEventListener('click', event => {
+                let button = event.target; //get the actual button id on click
+                button.innerText = 'Added to Bag'; // change button text
+                button.disabled = true; // disable button once item is clicked once
+                let productId = button.dataset.id; // get the id of the button that was clicked
+                let product = products.find(product => { // compare if the array id and the returned id are the same
+                    return product.id === productId;
+                });
+                //add items to local storage
+                let cartItems = localStorage.getItem('cartItems');
+                if (cartItems) {
+                    let items = JSON.parse(cartItems);
+                    items.push(product);// add product to the array
+                    localStorage.setItem('cartItems', JSON.stringify(items));
+
+                } else {
+                    let cartItem = JSON.stringify([product]); // convert an array into json string
+                    localStorage.setItem('cartItems', cartItem);// put it in local storage
+
+                }
             })
         })
     });
