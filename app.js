1e673dcf841d06c600ba3509a2e267650d2706b7
Alice <alicenjoroge707@gmail.com>
2020-02-08 18:38:24 +0300

    pass the products to the ui

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 let products = [];
 
- fetch('products.json')
+fetch('products.json')
     .then(response => response.json())
-    .then((data)=>{
+    .then((data) => {
         products = data;
         /*for loop
         for(let i=0;i<products.length; i++){
             let item = products[i];
         } */
-
-       /* foreach loop
+        /* foreach
        products.forEach((product,index)=> {
             console.log(product.title)
         }) */
-       for(let product of products){
-           console.log(product.price)
-       }
+
+        /* for of
+        for(let product of products){
+            console.log(product.price)
+        }
+         */
+        let productsHtmlString = '';
+
+
+        products.map((product, index) => {
+            let productString = `
+        <article class="product">
+            <div class="img-container">
+                <img src="${product.image}" alt="${product.title}" class="product-img">
+                <button class="bag-btn" data-id="${product.id}"><i class="fas fa-shopping-cart"></i>add to bag</button>
+            </div>
+            <h3>${product.title}</h3>
+            <h4>$${product.price}</h4>
+        </article>`
+            productsHtmlString += productString;
+        });
+        document.querySelector('.products-center').innerHTML = productsHtmlString;
+
 
     });
 
